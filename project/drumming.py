4e96c5945db11eb32a75bfb7d57ce8526bd84965
Yida Pan <yida.pan@mail.mcgill.ca>
2022-10-21 16:52:08 -0400

    Combine the lever and drumming class

--- project/drumming.py
+++ project/drumming.py
@@ -1,43 +1,97 @@
-from cgi import print_arguments
-from distutils.log import error
-from time import sleep
 import time
-from utils import sound
-from utils.brick import  wait_ready_sensors,Motor
+from enum import Enum
+
+from utils.brick import Motor
+
+
+class DrumMotor:
 # Initialization of the drumming motor and control motor
-DELAY_SEC = 0.05
-drum_motor =Motor("A")
-control_motor=Motor("B")
-drum_motor.reset_encoder #get the current position in degree,0 at the start
-control_motor.reset_encoder#get the current position in degree,0 at the start
-time.sleep(2) #wait for the initialization to finish
-wait_ready_sensors() # Note: Touch sensors actually have no initialization time
 
-def  drum_motor_init():
+     def __init__ (self, motor: Motor):
+       self.motor = motor
+
+     def drumming_on(self):
+          self.motor.set_power(50)
+     def drumming_off(self):
+          self.motor.set_power(0)
+
+
+
+
+class InputLever:
+    """
+    class InputLever.
+    The main purpose of this class to read the current position of the motor.
+    The current position of the motor will be used as an input to decide between:
+        1- Emergency stop (rotate between 0 degree and 119 degrees)
+        2- Stop drumming (rotate between 120 degrees and 239 degrees)
+        3- Start drumming (rotate between 240 degrees 360 degrees)
+    """
+
+    def __init__(self, motor: Motor):
+        """
+        The class constructor takes the motor as an input.
+        """
+        self.motor = motor
+
+    @property
+    def position(self):
+        return self.motor.get_position()
+
+    def get_state(self):
+        for state in LeverState:
+            if self.position in range(state.min_position, state.max_position):
+                return state
+        return None
+
+    def has_moved(self, delay):
+        last_position = self.motor.get_position()
+        time.sleep(delay)
+        current_position = self.motor.get_position()
+        return last_position != current_position
+
+
+class LeverState(Enum):
+    EMERGENCY_STOP = (-130,-70 )
+   # DRUMMING_OFF = (-34, 20)
+    DRUMMING_ON = (11, 130)
+    IDLE=(-69,10)
+
+    def __init__(self, min_position, max_position):
+        self.min_position = min_position
+        self.max_position = max_position
+
+
+if __name__ == "__main__":
+    DELAY = 0.2
+
+    lever = InputLever(Motor("A"))
+    drum  = DrumMotor(Motor("C"))
+
     try:
-        if(control_motor.get_position<90)and(control_motor.get_position>10):
-          drum_motor.set_power(40)
-        else:
-            print ("Please turn on the switch")
-  
-    except IOError as error:
-      print (error)  # capture all exceptions including KeyboardInterrupt (Ctrl-C)
-        
+        print("To start the program, ensure that you move the input lever to the emergency stop position")
+        while not lever.has_moved(DELAY):  # Waiting for lever to be moved to initial position
+            pass
+        while lever.has_moved(DELAY):  # Waiting for lever to stop moving
+            pass
+        lever.motor.reset_encoder()  # Resetting encoder based on initial set emergency stop lever position
+        lever_state = LeverState.IDLE
 
-def drum_on_switch_on():
-    "In an infinite loop, play a single note when the touch sensor is pressed."
-    try:
-        while True:
-            if (control_motor.get_position>90)or(control_motor.get_position<10):
-                 drum_motor.set_power(0)
-                 exit()
-                 
-            
-    except BaseException:  # capture all exceptions including KeyboardInterrupt (Ctrl-C)
-        exit()
+        print("start")
+        while(True):
+            print(lever_state)
+            if(lever_state==LeverState.DRUMMING_ON):
+                drum.drumming_on()
+                time.sleep(DELAY)      
 
+            elif (lever_state==LeverState.EMERGENCY_STOP):
+                drum.drumming_off()
+                time.sleep(DELAY)      
+            else:
+                 pass
+                
+            lever_state = lever.get_state()
 
-if __name__ == '__main__':
-   drum_motor_init
-   time.sleep(4)
-   drum_on_switch_on
+    except BaseException:
+         print(BaseException)
+         exit()
