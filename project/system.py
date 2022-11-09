0d3a819a1ed1d58eeddef2ed114233c1e83d7a5a
aptrott <adam-philippe.trottier@mail.mcgill.ca>
2022-11-08 20:13:33 -0500

    updated into cube grid class

--- project/system.py
+++ project/system.py
@@ -8,67 +8,72 @@ LOOP_INTERVAL = 0.500
 GRID_COLUMNS = 5
 GRID_ROWS = 5
 GRID_CELLS = GRID_COLUMNS * GRID_ROWS
-GRID_CELL_WIDTH = 4  # cm
-GRID_CELL_HEIGHT = 4  # cm
+MAXIMUM_CUBES = 15
+GRID_CELL_SIZE = 4  # cm
 
 
-def get_keyboard_binary_input_string():
-    max_length = GRID_CELLS
-    max_ones = 15
+class CubeGrid:
+
+    def __init__(self, user_input):
+        self.valid_binary_input = self.validate_binary_user_input(user_input)
+        self.grid: dict[list] = self.process_grid()
+
+    @staticmethod
+    def validate_binary_user_input(user_input):
+        if len(user_input) > GRID_CELLS:
+            raise Exception(
+                f'Input string is invalid, maximum length of {GRID_CELLS} exceeded ({len(user_input)} entered in total)')
+        if any([character not in "01" for character in user_input]):
+            raise Exception('Input string is invalid, only "1"s and "0"s are allowed')
+        if user_input.count("1") > MAXIMUM_CUBES:
+            raise Exception(
+                f'Input string is invalid, maximum of {MAXIMUM_CUBES} "1"s exceeded ({user_input.count("1")} entered in total)')
+        return user_input.ljust(GRID_CELLS, '0')
+
+    def process_grid(self):
+        grid = {k: [] for k in iter(range(1, GRID_COLUMNS + 1))}
+        for i, bit in enumerate(self.valid_binary_input):
+            column = (i % GRID_COLUMNS) + 1
+            if bit == "1":
+                row = GRID_ROWS - (i // GRID_ROWS)
+                row_list = grid.get(column)
+                row_list.append(row)
+                grid[column] = row_list
+        return grid
+
+    def get_cube_row_position(self, row):
+        return self.grid.get(row)
+
+    def preview_grid(self):
+        output = "-----------\n"
+        for row in range(GRID_ROWS, 0, -1):
+            output += "|"
+            for column in self.grid:
+                if row in self.grid[column]:
+                    output += "#|"
+                else:
+                    output += " |"
+            output += "\n"
+        output += "-----------\n"
+        print(output)
+
+
+def get_keyboard_binary_user_input():
     input_string = str(input(
-        f'Enter a string of "1"s and "0"s maximum length {max_length}, containing a maximum of {max_ones} "1"s:\n'))
-    input_string = input_string.replace(" ", "")
-    return validate_binary_input_string(input_string, max_length, max_ones)
-
-
-def validate_binary_input_string(input_string, max_length, max_ones):
-    if len(input_string) > max_length:
-        raise Exception(
-            f'Input string is invalid, maximum length of {max_length} exceeded ({len(input_string)} entered in total)')
-    if any([character not in "01" for character in input_string]):
-        raise Exception('Input string is invalid, only "1"s and "0"s are allowed')
-    if input_string.count("1") > max_ones:
-        raise Exception(
-            f'Input string is invalid, maximum of {max_ones} "1"s exceeded ({input_string.count("1")} entered in total)')
-    return input_string.ljust(max_length, '0')
-
-
-def process_binary_input_string(binary_input_string):
-    grid = {k: [] for k in iter(range(1, GRID_COLUMNS + 1))}
-    for i, bit in enumerate(binary_input_string):
-        column = i % GRID_COLUMNS + 1
-        if bit == "1":
-            row = GRID_ROWS - (i // GRID_ROWS)
-            row_list = grid.get(column)
-            row_list.append(row)
-            grid.update({column: row_list})
-    return grid
-
-
-def preview_grid(grid):
-    output = "-----------\n"
-    for row in range(GRID_ROWS, 0, -1):
-        output += "|"
-        for column in grid:
-            if row in grid[column]:
-                output += "#|"
-            else:
-                output += " |"
-        output += "\n"
-    output += "-----------\n"
-    return output
+        f'Enter a string of "1"s and "0"s maximum length {GRID_CELLS}, containing a maximum of {MAXIMUM_CUBES} "1"s:\n'))
+    return input_string.replace(" ", "")
 
 
 if __name__ == "__main__":
     try:
         while True:
             try:
-                s = get_keyboard_binary_input_string()
-                print(f'{s} ({s.count("1")} cubes required in total)')
-                g = process_binary_input_string(s)
-                print(g)
-                p = preview_grid(g)
-                print(p)
+                cube_grid = CubeGrid(get_keyboard_binary_user_input())
+                print(
+                    f'{cube_grid.valid_binary_input} ({cube_grid.valid_binary_input.count("1")} cubes required)')
+                print(cube_grid.grid)
+                cube_grid.preview_grid()
+
             except Exception as e:
                 print(e)
 
