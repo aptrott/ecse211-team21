a2ee4e57aa656e46919c2a99c4171130f294b730
Sirn0n <112601900+Sirn0n@users.noreply.github.com>
2022-11-15 13:21:28 -0500

    Update cube_pushing_piston.py
    if main added

--- project/cube_pushing_piston.py
+++ project/cube_pushing_piston.py
@@ -21,14 +21,24 @@ class Pushing_piston:
 
     def push(self,row):
         # setting 50% power to the motor in order to turn it on
-        rotation_angle = self.getAngle(row)
+        rotation_angle = self.getAngle(row,2)
         self.motor.set_power(50)
         self.motor.set_position_relative(rotation_angle)
         time.sleep(2)
         self.motor.set_position_relative(-rotation_angle)
 
-    
 
+
+if __name__ == "__main__":
+    pushing_motor = Pushing_piston(Motor("A"))
+    try:
+        j = 1
+        while j < 5:
+            Pushing_piston.push(j)
+            j += 1
+
+    except KeyboardInterrupt:
+        exit()
 
 
 
