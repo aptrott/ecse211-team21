73f8661530555f359dd4fdd7a07f63bc88f95369
aptrott <adam-philippe.trottier@mail.mcgill.ca>
2022-11-25 18:33:04 -0500

    added documentation to the final system code and refactored

--- project/system.py
+++ project/system.py
@@ -8,8 +8,6 @@ import threading
 LOOP_INTERVAL = 0.050
 SLEEP = 1
 
-# wait_ready_sensors(True)
-
 GRID_COLUMNS = 5
 GRID_ROWS = 5
 GRID_CELLS = GRID_COLUMNS * GRID_ROWS
@@ -18,36 +16,46 @@ GRID_CELL_SIZE = 4  # cm
 INITIAL_PUSHER_OFFSET = 1.5  # cm
 
 # Creating the 3 different to be played at each input.
-# each sound will confirm to the user whether they input '1' or '0' or 'complete'
+# Each sound will confirm to the user whether they input '1' or '0' or 'complete', or if the input is invalid.
 SOUND_1 = sound.Sound(duration=0.3, pitch="G6", volume=50)
 SOUND_0 = sound.Sound(duration=0.3, pitch="C6", volume=50)
 SOUND_COMPLETE = sound.Sound(duration=0.3, pitch="D6", volume=50)
 SOUND_INVALID = sound.Sound(duration=0.3, pitch="G5", volume=50)
-# Initiating the 2 different touch sensors for the two possible inputs '1' and '0'
-TS_1 = TouchSensor(4)
-TS_0 = TouchSensor(3)
-TS_COMPLETE = TouchSensor(2)
+
+# Creating the 3 different touch sensors for the two possible inputs '1' and '0' 'Complete'
+TOUCH_SENSOR_1 = TouchSensor(4)
+TOUCH_SENSOR_0 = TouchSensor(3)
+TOUCH_SENSOR_COMPLETE = TouchSensor(2)
+
+# Creating the 3 different motors
+ROBOT_MOVEMENT_MOTOR_1 = Motor("B")
+ROBOT_MOVEMENT_MOTOR_2 = Motor("C")
+PUSHER_MOTOR = Motor("D")
+
+wait_ready_sensors()
 
 
 class GridInputValidationError(Exception):
+    """Exception for invalid grid pattern inputs."""
+
     def __init__(self, message):
         super().__init__(message)
 
 
 class CubeGrid:
     def __init__(self, user_input):
+        """This class represents the cube grid that the robot needs to produce."""
         self.valid_binary_input = self.__validate_binary_user_input(user_input)
         self.grid = self.__process_grid()
 
-    def get_cubes_in_column(self, cube_column):
-        return self.grid.get(cube_column)
-
-    @staticmethod
-    def get_cube_position(current_cube):
-        return current_cube * GRID_CELL_SIZE
+    def get_cubes_in_column(self, column):
+        """This method returns a list of cubes to be placed in the given column."""
+        return self.grid.get(column)
 
     @staticmethod
     def __validate_binary_user_input(user_input):
+        """This private method validates the binary input string and pads it with 0s if it is shorter than the
+        required grid. An exception is raised if the input is invalid."""
         if len(user_input) > GRID_CELLS:
             raise GridInputValidationError(
                 f'Input string is invalid, maximum length of {GRID_CELLS} exceeded ({len(user_input)} entered in total)')
@@ -59,6 +67,8 @@ class CubeGrid:
         return user_input.ljust(GRID_CELLS, '0')
 
     def __process_grid(self):
+        """This private method uses the validated binary input string and processes it into a dictionary, with each key corresponding to a grid
+        column. The value associated to each key represents a list of rows of cubes to be placed in the column."""
         grid = {k: [] for k in iter(range(1, GRID_COLUMNS + 1))}
         for index, bit in enumerate(self.valid_binary_input):
             if bit == "1":
@@ -70,6 +80,7 @@ class CubeGrid:
         return grid
 
     def preview_grid(self):
+        """This method allows for a preview of the cubes on the grid to be printed to the terminal."""
         output = "_" * 16 + "\n"
         for row in range(GRID_ROWS, 0, -1):
             output += "|"
@@ -84,18 +95,25 @@ class CubeGrid:
 
 
 class UserInput:
-    def __init__(self):
+    def __init__(self, ts_0, ts_1, ts_complete):
+        """This class represents the binary user input string. Three touch sensors are used for this, as well as the terminal for keyboard input."""
+        self.ts_0 = ts_0
+        self.ts_1 = ts_1
+        self.ts_complete = ts_complete
         self.raw_user_input = ""
         self.is_input_complete = False
         self.is_using_touch_sensor_input = False
 
     def get_binary_user_input(self):
+        """This method is used to get binary user input from both the keyboard and the touch sensors at the same time using threading."""
         keyboard_input_thread = threading.Thread(target=self.__get_keyboard_binary_user_input, daemon=True)
         keyboard_input_thread.start()
         self.__get_touch_sensor_binary_user_input()
         return self.raw_user_input
 
     def __get_keyboard_binary_user_input(self):
+        """This private method is used to get binary user input from the terminal. If a touch sensor is pressed before this input is entered,
+        the touch sensor input is used instead. """
         user_input = str(input(
             f'Enter a string of "1"s and "0"s maximum length {GRID_CELLS}, containing a maximum of {MAXIMUM_CUBES} "1"s:\n'))
         if not self.is_using_touch_sensor_input:
@@ -104,88 +122,96 @@ class UserInput:
 
     @staticmethod
     def __wait_until_touch_sensor_released(touch_sensor):
+        """This private method is used to wait until a touch sensor is released until proceeding to read the following presses."""
         while touch_sensor.is_pressed():
             pass
 
     def __get_touch_sensor_binary_user_input(self):
+        """This private method is get the binary user input from the touch sensors."""
         while not self.is_input_complete:
             time.sleep(LOOP_INTERVAL)
-            if TS_0.is_pressed() or TS_1.is_pressed() or TS_COMPLETE.is_pressed():
+            if self.ts_0.is_pressed() or self.ts_1.is_pressed() or self.ts_complete.is_pressed():
                 self.is_using_touch_sensor_input = True
-            if TS_1.is_pressed() and not TS_0.is_pressed() and not TS_COMPLETE.is_pressed():
+            if self.ts_1.is_pressed() and not self.ts_0.is_pressed() and not self.ts_complete.is_pressed():
                 self.raw_user_input += "1"
                 SOUND_1.play()
-                self.__wait_until_touch_sensor_released(TS_1)
+                self.__wait_until_touch_sensor_released(self.ts_1)
                 SOUND_1.wait_done()
-            if TS_0.is_pressed() and not TS_1.is_pressed() and not TS_COMPLETE.is_pressed():
+            if self.ts_0.is_pressed() and not self.ts_1.is_pressed() and not self.ts_complete.is_pressed():
                 self.raw_user_input += "0"
                 SOUND_0.play()
-                self.__wait_until_touch_sensor_released(TS_0)
+                self.__wait_until_touch_sensor_released(self.ts_0)
                 SOUND_0.wait_done()
             if self.is_using_touch_sensor_input:
                 print(" " * 100, end="\r", flush=True)
                 print(f"\r{self.raw_user_input}", end="\r", flush=True)
-            if TS_COMPLETE.is_pressed() and not TS_1.is_pressed() and not TS_0.is_pressed():
+            if self.ts_complete.is_pressed() and not self.ts_1.is_pressed() and not self.ts_0.is_pressed():
                 self.is_input_complete = True
-                self.__wait_until_touch_sensor_released(TS_COMPLETE)
+                self.__wait_until_touch_sensor_released(self.ts_complete)
                 SOUND_COMPLETE.play()
                 SOUND_COMPLETE.wait_done()
                 print()
 
 
 class RobotMovement:
-    # Initialization of the motor
-
-    def __init__(self, motor: Motor, motor_2: Motor):
+    def __init__(self, motor_1: Motor, motor_2: Motor):
+        """This class represents robot movement for the column movement. Two motors are used for this."""
         self.initial_column = 1
-        self.motor = motor
+        self.motor_1 = motor_1
         self.motor_2 = motor_2
-        self.motor.set_limits(dps=100)
+        self.motor_1.set_limits(dps=100)
         self.motor_2.set_limits(dps=100)
         self.current_column = self.initial_column
 
     @staticmethod
     def get_rotation_angle(linear_distance):
+        """This method returns the rotation angle calculation for a given linear distance."""
         radius = 2.00
         angle = (360 * linear_distance) / (2 * math.pi * radius)
         return angle
 
     def move(self, column):
-        self.motor.reset_encoder()
+        """This method moves the robot from the current column to the given column."""
+        self.motor_1.reset_encoder()
+        self.motor_2.reset_encoder()
         distance = 4 * (column - self.current_column)
         angle = self.get_rotation_angle(distance)
-        self.motor.set_position_relative(angle)
+        self.motor_1.set_position_relative(angle)
         self.motor_2.set_position_relative(angle)
         time.sleep(SLEEP)
-        self.motor.wait_is_stopped()
+        self.motor_1.wait_is_stopped()
         self.motor_2.wait_is_stopped()
         self.current_column = column
 
     def return_to_initial(self):
-        self.motor.reset_encoder()
+        """This method moves the robot back to it's initial position."""
+        self.motor_1.reset_encoder()
+        self.motor_2.reset_encoder()
         distance = 4 * (self.current_column - self.initial_column)
         angle = -self.get_rotation_angle(distance)
-        self.motor.set_position_relative(angle)
+        self.motor_1.set_position_relative(angle)
         self.motor_2.set_position_relative(angle)
         time.sleep(SLEEP)
-        self.motor.wait_is_stopped()
+        self.motor_1.wait_is_stopped()
         self.motor_2.wait_is_stopped()
         self.current_column = self.initial_column
 
 
 class Pusher:
-    # Initialization of the motor
 
     def __init__(self, motor: Motor):
+        """This class represents the pusher for the row movement. One motor is used for this."""
         self.motor = motor
 
     @staticmethod
     def get_rotation_angle(linear_distance):
+        """This method returns the rotation angle calculation for a given linear distance."""
         radius = 2.05
         angle = (360 * linear_distance) / (2 * math.pi * radius)
         return angle
 
     def push(self, row):
+        """This method pushes a cube to the given row."""
         self.motor.set_limits(dps=200)
         distance = 4 * row
         self.motor.reset_encoder()
@@ -200,9 +226,10 @@ class Pusher:
         self.motor.wait_is_stopped()
 
     def load_cube(self):
+        """This method loads a cube into the pushing mechanism."""
         self.motor.set_limits(dps=90)
         load_distance = 6
-        ready_to_push_distance = - (6 - INITIAL_PUSHER_OFFSET)
+        ready_to_push_distance = - (load_distance - INITIAL_PUSHER_OFFSET)
         self.motor.reset_encoder()
         load_rotation_angle = self.get_rotation_angle(load_distance)
         ready_to_push_rotation_angle = self.get_rotation_angle(ready_to_push_distance)
@@ -217,7 +244,7 @@ class Pusher:
 
 if __name__ == "__main__":
     try:
-        input_string = UserInput().get_binary_user_input()
+        input_string = UserInput(TOUCH_SENSOR_1, TOUCH_SENSOR_0, TOUCH_SENSOR_COMPLETE).get_binary_user_input()
         try:
             cube_grid = CubeGrid(input_string)
         except GridInputValidationError as e:
@@ -231,13 +258,13 @@ if __name__ == "__main__":
             print(cube_grid.grid)
             cube_grid.preview_grid()
 
-            robot_movement = RobotMovement(Motor("B"), Motor("C"))
-            pusher = Pusher(Motor("D"))
-            for column in cube_grid.grid:
-                print(f"moving to column {column}")
-                if cube_grid.get_cubes_in_column(column):
-                    robot_movement.move(column)
-                for cube_row in cube_grid.get_cubes_in_column(column):
+            robot_movement = RobotMovement(ROBOT_MOVEMENT_MOTOR_1, ROBOT_MOVEMENT_MOTOR_2)
+            pusher = Pusher(PUSHER_MOTOR)
+            for cube_column in cube_grid.grid:
+                print(f"moving to column {cube_column}")
+                if cube_grid.get_cubes_in_column(cube_column):
+                    robot_movement.move(cube_column)
+                for cube_row in cube_grid.get_cubes_in_column(cube_column):
                     print("loading cube")
                     pusher.load_cube()
                     print(f"pushing cube to row {cube_row}")
@@ -246,5 +273,5 @@ if __name__ == "__main__":
             print(f"returning to initial position {robot_movement.initial_column}")
 
     except KeyboardInterrupt:
-        # reset_brick()
+        reset_brick()
         exit(0)
